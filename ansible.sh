3830632ae4ec395bde0c229d73bbddd38e691ea3
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-03-24 23:59:09 +0530

    GoCD Pipeline for frontend

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,3 @@
 component=$1
-env=$2
+env=dev
 #ansible-playbook -i $component-dev.rdevops6a.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=$env -e role_name=$component expense.yml
